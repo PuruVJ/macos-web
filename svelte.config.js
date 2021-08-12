864abde537da04f503e100063c29060eae10b591
Puru Vijay <devpuruvj@gmail.com>
2021-08-12 23:42:19 +0530

    Streamline svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import sveltePreprocess from 'svelte-preprocess'
+import sveltePreprocess from 'svelte-preprocess';
 
 export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
+  preprocess: sveltePreprocess(),
+};

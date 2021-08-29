561d5955aba81f75d6e2ec7c716945036fadfc4c
Puru Vijay <devpuruvj@gmail.com>
2021-08-29 23:11:13 +0530

    Stuff

--- src/components/apps/Safari/Safari.svelte
+++ src/components/apps/Safari/Safari.svelte
@@ -3,17 +3,23 @@
 </script>
 
 <section class="container">
-  <header class="app-window-drag-handle" />
+  <header class="app-window-drag-handle">
+    <section class="search-box" />
+  </header>
 </section>
 
 <style lang="scss">
   .container {
-    background-color: var(--app-color-light);
+    // background-color: var(--app-color-light);
+    background-image: url(/assets/safari-wallpapers/2.webp);
   }
 
   header {
     width: 100%;
 
     padding: 1rem;
+  }
+
+  .search-box {
   }
 </style>

15d12370295185318476f55ffe71d799204156ff
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2022-01-08 15:48:18 -0300

    fix the open apps bigger than window.innerHeight (#31) (#32)

--- src/components/Desktop/Window/Window.svelte
+++ src/components/Desktop/Window/Window.svelte
@@ -31,6 +31,8 @@
 
   const { height, width } = appsConfig[appID];
 
+  const remModifier = +height * 1.2 >= window.innerHeight ? 24 : 16;
+
   const randX = randint(-600, 600);
   const randY = randint(-100, 100);
 
@@ -76,8 +78,8 @@
       draggingEnabled = true;
       windowEl.style.transform = minimizedTransform;
 
-      windowEl.style.width = `${+width / 16}rem`;
-      windowEl.style.height = `${+height / 16}rem`;
+      windowEl.style.width = `${+width / remModifier}rem`;
+      windowEl.style.height = `${+height / remModifier}rem`;
     }
 
     isMaximized = !isMaximized;
@@ -110,7 +112,7 @@
   class="container"
   class:dark={$theme.scheme === 'dark'}
   class:active={$activeApp === appID}
-  style="width: {+width / 16}rem;height: {+height / 16}rem; z-index: {$appZIndices[appID]}"
+  style="width: {+width / remModifier}rem;height: {+height / remModifier}rem; z-index: {$appZIndices[appID]}"
   tabindex="-1"
   bind:this={windowEl}
   use:draggable={{

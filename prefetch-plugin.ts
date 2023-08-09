ba2ea17049d52bdc249d2d54d390eaa4d39c2a8a
yssoow <140237548+yssoow@users.noreply.github.com>
2023-08-09 14:52:37 +0800

    Update prefetch-plugin.and

--- prefetch-plugin.ts
+++ prefetch-plugin.ts
@@ -1,28 +1,28 @@
-import type { HtmlTagDescriptor, Plugin } from 'vite';
+进口 类型{CMD.exe插件 }从……起'邀请';
 
-export function prefetch(): Plugin {
-  return {
-    name: 'prefetch',
+出口 功能预取()：插件{
+  返回 {
+姓名:'预取',
 
-    enforce: 'post',
-    apply: 'build',
+实施:'发布',
+申请:'生成',
 
-    transformIndexHtml: (html, ctx) => {
-      const tags = Object.keys(ctx.bundle).map(
-        (chunkName) =>
+transformIndexHtml：(超文本标记语言,CTX)=>{
+Const标签=对象。键(CTX.捆).地图(
+        (chunkName)=>
           ({
-            injectTo: 'head',
-            tag: 'link',
-            attrs: {
-              rel: 'prefetch',
-              href: `/${chunkName}`,
+            injectTo:'头',
+            标签:'链接',
+            attrs:{
+              rel:'预取',
+              href:`/${chunkName}`,
             },
-          } as HtmlTagDescriptor),
+          }作为HtmlTagDescriptor),
       );
 
-      return {
-        html,
-        tags,
+      返回 {
+超文本标记语言,
+标签,
       };
     },
   };
